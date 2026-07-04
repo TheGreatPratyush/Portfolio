317c94cca18f366a955ae04c55d7ed1d3958beb2
The GreatPratyush <pratushgupta27199@gmail.com>
2026-07-04 13:16:56 +0530

    Loading page to Page 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import LoadingPage from './Pages/LoadingPages'
-import ProjectChaos from './Pages/RandomImages'
+import React, { useEffect, useState } from "react";
+
+import LoadingPage from "./Pages/LoadingPages";
+import Slide1 from "./Slides/Slide1/Slide1";
+import Navbar from "./components/Navbar/Navbar";
+import SlideIndicator from "./components/SlideIndicator/SlideIndicator";
 
 const App = () => {
-  return (
-    <div>
-      <LoadingPage/>
-      {/* <ProjectChaos/> */}
-    </div>
-  )
-}
 
-export default App
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 8000); 
+
+    return () => clearTimeout(timer);
+
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
+  return (
+    <>
+      <SlideIndicator />
+      <Slide1 />
+      <Navbar />
+    </>
+  );
+};
+
+export default App;

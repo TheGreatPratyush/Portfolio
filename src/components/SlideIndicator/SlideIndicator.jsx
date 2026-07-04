b2d8741459c0fd9e87057c5cdb9f3f3da8ed9acb
The GreatPratyush <pratushgupta27199@gmail.com>
2026-07-04 13:18:04 +0530

    current Slide Indicator

---
+++ src/components/SlideIndicator/SlideIndicator.jsx
@@ -0,0 +1,45 @@
+import "./SlideIndicator.css";
+
+const SlideIndicator = () => {
+  return (
+    <div className="slide-indicator">
+
+      <div className="indicator-item active">
+        <div className="indicator-dot"></div>
+      </div>
+
+      <div className="indicator-line"></div>
+
+      <div className="indicator-item">
+        <div className="indicator-dot"></div>
+      </div>
+
+      <div className="indicator-line"></div>
+
+      <div className="indicator-item">
+        <div className="indicator-dot"></div>
+      </div>
+
+      <div className="indicator-line"></div>
+
+      <div className="indicator-item">
+        <div className="indicator-dot"></div>
+      </div>
+
+      <div className="indicator-line"></div>
+
+      <div className="indicator-item">
+        <div className="indicator-dot"></div>
+      </div>
+
+      <div className="indicator-line"></div>
+
+      <div className="indicator-item">
+        <div className="indicator-dot"></div>
+      </div>
+
+    </div>
+  );
+};
+
+export default SlideIndicator;
